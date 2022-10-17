22448a5fe6f6ed30a9e4aadb3dda8f1a8d34c14c
ShaD <Danec2004@gmail.com>
2022-10-17 04:33:56 +0000

    1017

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=
+	, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<style>
+	.columns {
+  display: flex;
+}
+
+.column {
+  flex: 1;
+}
+
+.main-column {
+  flex: 2;
+}
+</style>
+<body>
+<div class="columns">
+  <div class="column main-column">
+    2/3 column
+  </div>
+  <div class="column">
+    1/3 column
+  </div>
+</div>
+</body>
+</html>
